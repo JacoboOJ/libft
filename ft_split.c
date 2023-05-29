b27a5cb1d8e90d18f4023133adfedff7374a00ff
Jacobo Orozco Jaramillo <JacoOJ@MacBook-Pro-de-Mario-3.local>
2023-05-30 01:51:14 +0200

    split

---
+++ ft_split.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaorozco <jaorozco@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/19 19:51:40 by jaorozco          #+#    #+#             */
+/*   Updated: 2023/05/30 01:49:47 by jaorozco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	count_words(const char *s, char c)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	while (s[i] != '\0')
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i])
+			count ++;
+		while (s[i] != c && s[i])
+			i++;
+	}
+	return (count);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	char	**new;
+	size_t	i;
+	size_t	len;
+
+	new = malloc(sizeof(char *) * count_words(s, c) + 1);
+	if (!s || !new)
+		return (0);
+	i = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			len = 0;
+			while (*s && *s != c && ++len)
+				s++;
+			new[i++] = ft_substr(s - len, 0, len);
+		}
+		else
+			++s;
+	}
+	new[i] = 0;
+	return (new);
+}
+/*
+int	main()
+{
+	int	i = 0;
+	char	**strs;
+
+	strs = ft_split("Hola Como estas?", ' ');
+	while(i < count_words("Hola Como estas?", ' '))
+	{
+		printf("%i - %s\n", i, strs[i]);
+		i++;
+	}
+}*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jaorozco <jaorozco@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 13:57:44 by jaorozco          #+#    #+#             */
-/*   Updated: 2023/05/19 17:25:19 by jaorozco         ###   ########.fr       */
+/*   Updated: 2023/05/30 01:39:40 by jaorozco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,6 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(const char *s, unsigned int start, size_t len);
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_strtrim(const char *s1, const char *set);
+char	**ft_split(const char *S, char c);
 
 #endif
